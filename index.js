aaebbd4f6877912d6272ab252a08b8fb4d7f4f50
Jikky1618 <Jikky0110@gmail.com>
2022-09-21 09:40:29 +0900

    不要なintentsを削除

--- index.js
+++ index.js
@@ -6,20 +6,13 @@ const client = new Client({
     intents: [
         "GUILDS",
         "GUILD_MEMBERS",
-        "GUILD_BANS",
         "GUILD_EMOJIS_AND_STICKERS",
         "GUILD_INTEGRATIONS",
-        "GUILD_WEBHOOKS",
-        "GUILD_INVITES",
-        "GUILD_VOICE_STATES",
         "GUILD_PRESENCES",
         "GUILD_MESSAGES",
         "GUILD_MESSAGE_REACTIONS",
         "GUILD_MESSAGE_TYPING",
-        "DIRECT_MESSAGES",
-        "DIRECT_MESSAGE_REACTIONS",
-        "DIRECT_MESSAGE_TYPING",
-    ]
+    ],
 });
 const MY_GUILD = config.serverId;
 require("dotenv").config();
